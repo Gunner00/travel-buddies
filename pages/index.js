a48c1e9dabc1d046effadd3b68407b2f778b5fcf
Gunner00 <asad.rana2000@gmail.com>
2023-05-25 00:28:54 +0500

    fyp

--- pages/index.js
+++ pages/index.js
@@ -6,13 +6,13 @@ import dotenv from 'dotenv';
 export default function Home() {
 
     return (
-        <LoadScript googleMapsApiKey={process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY}>
+        <LoadScript googleMapsApiKey=       {process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY}>
   <GoogleMap
     mapContainerStyle={{ width: '100%', height: '400px' }}
     center={{ lat: 37.7749, lng: -122.4194 }} // Replace with your desired center coordinates
     zoom={10} // Adjust the zoom level as needed
   >
-    <Marker position={{ lat: 37.7749, lng: -122.4194 }} /> // Replace with your desired marker coordinates
+    <Marker position={{ lat: 37.7749, lng: -122.4194 }} />
   </GoogleMap>
 </LoadScript>
 
