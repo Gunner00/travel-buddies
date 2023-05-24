5ecb4f3bb922b35cf84a02a6e65a85bf6e6ba0e8
Gunner00 <asad.rana2000@gmail.com>
2023-05-25 02:31:36 +0500

    fyp

--- pages/index.js
+++ pages/index.js
@@ -8,7 +8,6 @@ export default function Home() {
         <LoadScript googleMapsApiKey=       {process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY}>
   <GoogleMap
     mapContainerStyle={{ width: '100%', height: '400px' }}
-    center={{ lat: 37.7749, lng: -122.4194 }} // Replace with your desired center coordinates
     zoom={10} // Adjust the zoom level as needed
   >
     <Marker position={{ lat: 37.7749, lng: -122.4194 }} />
