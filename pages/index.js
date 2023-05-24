14fe89fb1a0ece2f82635cf348b0b36de5414bfe
Gunner00 <asad.rana2000@gmail.com>
2023-05-25 02:04:04 +0500

    fyp

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,20 @@
 import SplashScreen from "../components/SplashScreen";
+import { GoogleMap, LoadScript, Marker } from '@react-google-maps/api';
+import dotenv from 'dotenv';
 
 
 export default function Home() {
 
     return (
-        <div className="">
-            <SplashScreen />
-        </div>
+        <LoadScript googleMapsApiKey=       {process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY}>
+  <GoogleMap
+    mapContainerStyle={{ width: '100%', height: '400px' }}
+    center={{ lat: 37.7749, lng: -122.4194 }} // Replace with your desired center coordinates
+    zoom={10} // Adjust the zoom level as needed
+  >
+    <Marker position={{ lat: 37.7749, lng: -122.4194 }} />
+  </GoogleMap>
+</LoadScript>
+
     );
 }
