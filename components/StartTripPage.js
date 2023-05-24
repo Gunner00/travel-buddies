5174edb0bf9516a6eb54c897115c9755f1faa2af
Gunner00 <asad.rana2000@gmail.com>
2023-05-24 22:40:35 +0500

    fyp

--- components/StartTripPage.js
+++ components/StartTripPage.js
@@ -27,7 +27,7 @@ export default function StartTripPage() {
     const destinationsRef = useRef();
 
     const { isLoaded } = useLoadScript({
-        googleMapsApiKey: process.env.NEXT_PUBLIC_GOOGLE_API_KEY,
+        googleMapsApiKey: "AIzaSyDVRRRtrryvgO1teR3oPktKzkL3bAEmf0U",
         libraries: ["places"],
     });
     /* Calling the getCurrentUser function when the session changes. */
--- components/TripMatchingPage.js
+++ components/TripMatchingPage.js
@@ -24,7 +24,7 @@ export default function TripMatchingPage() {
     const [userTripRequestID, setUserTripRequestID] = useState(null);
 
     const { isLoaded } = useLoadScript({
-        googleMapsApiKey: process.env.NEXT_PUBLIC_GOOGLE_API_KEY,
+        googleMapsApiKey: "AIzaSyDVRRRtrryvgO1teR3oPktKzkL3bAEmf0U",
         libraries: ["places", "geometry"],
     });
 
