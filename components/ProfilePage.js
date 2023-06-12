68c48f61454a47b385ce7af3e7ae4e8bfa9bdf52
Gunner00 <asad.rana2000@gmail.com>
2023-06-13 00:15:35 +0500

    fyp

--- components/ProfilePage.js
+++ components/ProfilePage.js
@@ -1,6 +1,6 @@
 import { useState, useEffect } from "react";
 import { supabase } from "../utils/supabaseClient";
-import Avatar from "./Avatar"
+import Avatar from "./Avatar";
 import { useRouter } from "next/router";
 import toast from "react-hot-toast";
 import UniversalFadeAnimation from "./UniversalFadeComponent";
@@ -18,7 +18,6 @@ export default function Account({ session }) {
     const router = useRouter();
 
     useEffect(() => {
-        console.log(session);
         getProfile();
     }, []);
 
@@ -169,7 +168,6 @@ export default function Account({ session }) {
             setLoading(false);
         }
     };
-
 
     return (
         <>
