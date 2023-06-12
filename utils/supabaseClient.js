0af2dbedefc307334e96ab74f4223957de14e76a
Gunner00 <asad.rana2000@gmail.com>
2023-06-13 01:42:14 +0500

    fyp

--- utils/supabaseClient.js
+++ utils/supabaseClient.js
@@ -1,4 +1,4 @@
-import { createClient } from '@supabase/supabase-js'
+import { createClient } from "@supabase/supabase-js";
 
 const supabaseUrl = "https://xksxmfrhnucxpduxkpmm.supabase.co"
 const supabaseAnonKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inhrc3htZnJobnVjeHBkdXhrcG1tIiwicm9sZSI6ImFub24iLCJpYXQiOjE2ODYwODIzMDQsImV4cCI6MjAwMTY1ODMwNH0.NDlbuwxjuOuZVQswB-RmkwwGxIIzrkFqi5u62Xqy90E"
