3c258bd18849dcbe64999e44460e276bebaede04
Gunner00 <asad.rana2000@gmail.com>
2023-06-13 22:48:14 +0500

    fyp

--- utils/supabaseClient.js
+++ utils/supabaseClient.js
@@ -1,6 +1,5 @@
-import { createClient } from "@supabase/supabase-js";
+import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = "https://xksxmfrhnucxpduxkpmm.supabase.co";
-const supabaseAnonKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inhrc3htZnJobnVjeHBkdXhrcG1tIiwicm9sZSI6ImFub24iLCJpYXQiOjE2ODYwODIzMDQsImV4cCI6MjAwMTY1ODMwNH0.NDlbuwxjuOuZVQswB-RmkwwGxIIzrkFqi5u62Xqy90E";
-
-export const supabase = createClient(supabaseUrl, supabaseAnonKey)
+const supabaseUrl = 'https://xksxmfrhnucxpduxkpmm.supabase.co'
+const supabaseKey = process.env.SUPABASE_KEY
+const supabase = createClient(supabaseUrl, supabaseKey)
