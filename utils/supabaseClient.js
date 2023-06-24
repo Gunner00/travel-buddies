5971ceb636786f93b7726640a724b4818ac6c76b
Gunner00 <asad.rana2000@gmail.com>
2023-06-25 00:15:53 +0500

    fyp

--- utils/supabaseClient.js
+++ utils/supabaseClient.js
@@ -2,6 +2,6 @@
 import { createClient } from '@supabase/supabase-js'
 
 const supabaseUrl = "https://xksxmfrhnucxpduxkpmm.supabase.co"
-const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
+const supabaseAnonKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inhrc3htZnJobnVjeHBkdXhrcG1tIiwicm9sZSI6ImFub24iLCJpYXQiOjE2ODYwODIzMDQsImV4cCI6MjAwMTY1ODMwNH0.NDlbuwxjuOuZVQswB-RmkwwGxIIzrkFqi5u62Xqy90E"
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey)
