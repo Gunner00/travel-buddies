197c2ed03e71d784c45c8a0e360eea749891ca7e
Gunner00 <asad.rana2000@gmail.com>
2023-07-01 20:24:23 +0500

    fyp

--- components/ProfilePage.js
+++ components/ProfilePage.js
@@ -125,11 +125,12 @@ export default function Account({ session }) {
 
     let handleGetStartedClick = async () => {
         let userFilledInfoDatabase = await checkIfUserFilledInfo();
-        if (userFilledInfoDatabase == true) {
+        if (userFilledInfoDatabase) {
             router.push("/startTripPage");
         } else {
             toast.error("Please fill in all the information!");
         }
+        console.log("i am accessing");
     };
       
 
