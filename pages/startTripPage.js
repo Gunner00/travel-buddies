c27588388985b1f75410fff620a27b0aca2f576e
Gunner00 <asad.rana2000@gmail.com>
2023-07-03 00:45:02 +0500

    fyp

--- pages/startTripPage.js
+++ pages/startTripPage.js
@@ -1,8 +1,12 @@
 import React from 'react'
 import StartTripPage from '../components/StartTripPage'
+import ChatComponent from '../components/ChatComponent';
 
 export default function startTripPage() {
   return (
     <StartTripPage />
   )
 }
+const [showChat, setShowChat] = useState(false);
+<button onClick={() => setShowChat(true)}>Open Chat</button>
+{showChat && <ChatComponent currentTrip={currentTrip} />}
