fb19595a168a0122e8f0af2a32110357cd4ba5a5
Gunner00 <asad.rana2000@gmail.com>
2023-07-03 00:53:05 +0500

    fyp

--- pages/startTripPage.js
+++ pages/startTripPage.js
@@ -1,6 +1,5 @@
 import React from 'react'
 import StartTripPage from '../components/StartTripPage'
-import ChatComponent from '../components/ChatComponent';
 
 export default function startTripPage() {
   return (
