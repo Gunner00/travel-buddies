0f5aed012e38ed9cf6c71b0c169ec205df1a80d0
Gunner00 <asad.rana2000@gmail.com>
2023-07-06 23:13:11 +0500

    fyp

--- components/ProfilePage.js
+++ components/ProfilePage.js
@@ -188,7 +188,7 @@ export default function Account({ session }) {
                                 "You're good to go! Click the button to get started."}
                         </h1>
                     </section>
-                    <section className="flex flex-col w-full lg:flex-row gap-10 justify-center items-center md:w-1/2">
+                    <section className="flex flex-col w-full lg:flex-row gap-10 justify-center items-center md:w-1/2inline-flex rounded-md border border-transparent bg-blue-600 px-4 py-2 text-sm font-medium text-white shadow-sm hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2">
                         <Avatar
                             url={avatar_url}
                             size={150}
