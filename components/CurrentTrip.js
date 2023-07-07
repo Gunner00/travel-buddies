c13b682aef7900258b6ee62c6db990ea0eca4311
Gunner00 <asad.rana2000@gmail.com>
2023-07-08 03:57:23 +0500

    fyp

--- components/CurrentTrip.js
+++ components/CurrentTrip.js
@@ -175,7 +175,7 @@ export default function CurrentTrip() {
     }
 //opens the chat feature upon click
     const handleChat = () => {
-        router.push("/chat.html");
+        document.open("/chat.html");
     }
     return (
         <>
