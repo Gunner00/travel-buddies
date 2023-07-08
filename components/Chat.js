aa724f9e087aeab6b89aa6a0eb027f4e9b6b5409
Gunner00 <asad.rana2000@gmail.com>
2023-07-09 00:56:45 +0500

    fyp

--- components/Chat.js
+++ components/Chat.js
@@ -1,73 +1,119 @@
+import { useState, useEffect } from 'react';
 import { supabase } from '../utils/supabaseClient';
 import Avatar from './Avatar';
 
-const messagesElement = document.querySelector('#messages');
+const Chat = () => {
+  const [messages, setMessages] = useState([]);
+  const [username, setUsername] = useState('');
+  const [content, setContent] = useState('');
 
-function addMessageToPage(message) {
-    const element = document.createElement('li');
-    element.classList.add('card','m-2');
-    element.innerHTML = `
-    <div class="card-body">
-        <div class="row"
-            <div class="col-sm-2 avatar-container">
-                <img src="${<Avatar
-                    url={buddy?.avatar_url}
+  useEffect(() => {
+    fetchMessages();
+    subscribeToMessages();
+  }, []);
+
+  const fetchMessages = async () => {
+    try {
+      const { data, error } = await supabase.from('messages').select('*');
+      if (error) {
+        console.error('Error fetching messages:', error);
+      } else {
+        setMessages(data);
+      }
+    } catch (error) {
+      console.error('Error fetching messages:', error);
+    }
+  };
+
+  const subscribeToMessages = () => {
+    supabase
+      .from('messages')
+      .on('INSERT', (payload) => {
+        setMessages((prevMessages) => [...prevMessages, payload.new]);
+      })
+      .subscribe();
+  };
+
+  const addMessage = async () => {
+    if (!username || !content) {
+      return;
+    }
+
+    const newMessage = {
+      username,
+      content,
+    };
+
+    try {
+      await supabase.from('messages').insert(newMessage);
+      setContent('');
+    } catch (error) {
+      console.error('Error sending message:', error);
+    }
+  };
+
+  return (
+    <div className="container main-area">
+      <ul className="list-unstyled" id="messages">
+        {messages.map((message) => (
+          <li key={message.id} className="card m-2">
+            <div className="card-body">
+              <div className="row">
+                <div className="col-sm-2 avatar-container">
+                  <Avatar
+                    url={message.avatar_url}
                     className="w-20 h-20 rounded-full"
                     showUpload={false}
-                />}">
-                <p class="avatar-username">${message.username}</p>
+                  />
+                  <p className="avatar-username">{message.username}</p>
+                </div>
+                <div className="col-sm-10">
+                  <p>{message.content}</p>
+                </div>
+              </div>
+              <div className="row">
+                <p className="col-sm-12 timestamp">{message.created_at}</p>
+              </div>
             </div>
-            <div class="col-sm-10>
-                <p>${message.content}</p>
-            </div>
-        </div>
-        <div class="row">
-            <p class="col-sm-12 timestamp">${message.created_at}</p>
-        </div>
+          </li>
+        ))}
+      </ul>
+      <form className="form" onSubmit={addMessage}>
+        <fieldset>
+          <div className="form-group">
+            <label htmlFor="username" className="form-label mt-4">
+              Username
+            </label>
+            <input
+              name="username"
+              type="text"
+              className="form-control"
+              id="username"
+              placeholder="Enter username"
+              value={username}
+              onChange={(e) => setUsername(e.target.value)}
+            />
+          </div>
+          <div className="form-group">
+            <label htmlFor="content" className="form-label mt-4">
+              Message
+            </label>
+            <textarea
+              name="content"
+              className="form-control"
+              id="content"
+              rows="2"
+              value={content}
+              onChange={(e) => setContent(e.target.value)}
+            ></textarea>
+          </div>
+          <button type="submit" className="btn btn-primary">
+            Send
+          </button>
+        </fieldset>
+      </form>
     </div>
-    `;
-    messagesElement.append(message);
+  );
+};
 
-    setTimeout(() => {
-        element.scrollIntoView({ behavior: 'smooth' });
-    }, 300)
-    
-}
-
-const form = document.querySelector('form');
-const contentElement = document.querySelector('#content');
-
-async function init() {
-   
-    form.addEventListener('submit', (event) => {
-        event.preventDefault();
-        const formData = new FormData(form);
-        const message = {
-            username: formData.get('username'),
-            content: formData.get('content'),
-        };
-        contentElement.value = '';
-        supabase
-            .from('messages')
-            .insert([
-                message,
-            ]).then(() => {
-                console.log('Message Sent!');
-            });
-    });
-
-    const { data: messages, error } = await supabase
-    .from('messages')
-    .select('*');
-
-messages.forEach(addMessageToPage);
-
-supabase
-    .from('messages')
-    .on('INSERT', (message) => {
-        addMessageToPage(message.new);
-    })
-    .subscribe();
-}
-
-init();
+export default Chat;
---
+++ pages/chat.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import Head from 'next/head';
+import dynamic from 'next/dynamic';
+
+const ChatPage = dynamic(() => import('../components/Chat'), { ssr: false });
+
+const Chat = () => (
+  <>
+    <Head>
+      <title>Chat</title>
+      <link href="https://bootswatch.com/5/litera/bootstrap.css" rel="stylesheet" />
+      <link rel="stylesheet" href="../styles/styles.css" />
+    </Head>
+    <ChatPage />
+  </>
+);
+
+export default Chat;
