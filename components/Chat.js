e8bbe8b11caa794c8c100bddee0f2e067cd374f6
Gunner00 <asad.rana2000@gmail.com>
2023-07-09 02:16:45 +0500

    fyp

--- components/Chat.js
+++ components/Chat.js
@@ -55,11 +55,11 @@ const Chat = () => {
 
   return (
     <div className="container main-area">
-      <ul className="list-unstyled" id="messages">
+      <ul id="messages">
         {messages.map((message) => (
           <li key={message.id} className="card m-2">
-            <div className="card-body">
-              <div className="row">
+            <div>
+              <div>
                 <div className="col-sm-2 avatar-container">
                   <Avatar
                     url={message.avatar_url}
@@ -72,7 +72,7 @@ const Chat = () => {
                   <p>{message.content}</p>
                 </div>
               </div>
-              <div className="row">
+              <div>
                 <p className="col-sm-12 timestamp">{message.created_at}</p>
               </div>
             </div>
@@ -81,8 +81,8 @@ const Chat = () => {
       </ul>
       <form className="flex flex-col gap-4 w-full sm:w-60" onSubmit={addMessage}>
         <fieldset>
-          <div className="form-group">
-            <label htmlFor="username" className="form-label mt-4">
+          <div>
+            <label htmlFor="username" className="mt-4">
               Username
             </label>
             <input
