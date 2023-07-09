f7acf0d2ca54a0ac12dd1d5ab8d32ffbeefdab59
Gunner00 <asad.rana2000@gmail.com>
2023-07-10 04:05:18 +0500

    fyp

--- components/Avatar.js
+++ components/Avatar.js
@@ -66,7 +66,7 @@ export default function Avatar({ url, size, onUpload, showUpload = true, classNa
             )}
             {showUpload && (
                 <div className="mt-2">
-                    <button className="interBody" htmlFor="single">
+                    <button className="inline-flex items-center rounded-md border border-transparent bg-blue-600 px-4 py-2 text-sm font-medium text-white shadow-sm hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2" htmlFor="single">
                         {uploading ? "Uploading ..." : "Upload a file"}
                     </button>
                     <input
