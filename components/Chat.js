759c1930eac7cb29f4029f90c28eff2c4e0a9ff0
Gunner00 <asad.rana2000@gmail.com>
2023-07-10 03:28:28 +0500

    fyp

--- components/Chat.js
+++ components/Chat.js
@@ -1,4 +1,4 @@
-import { React, useState, useEffect } from 'react';
+import { React, useRef, useState, useEffect } from 'react';
 import { supabase } from '../utils/supabaseClient';
 
 const Chat = () => {
